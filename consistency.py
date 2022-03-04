70f1e5722cc8404170f4919c89e0ab4f4d13e3c7
Perizat Menard <perizat.m@haystacks.ai>
2022-03-04 17:11:34 -0500

    consystency

---
+++ consistency.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+airlines = pd.read_csv("/Users/perizatmenard/Documents/advanced_python/cleaning_data_with_pandas/data/airlines_final.csv")
+
+
+# Find the cleanliness category in airlines not in categories
+cat_clean = set(airlines['cleanliness']).difference(categories['cleanliness'])
+
+# Find rows with that category
+cat_clean_rows = airlines['cleanliness'].isin(cat_clean)
+
+# Print rows with inconsistent category
+print(airlines[cat_clean_rows])
+
+# Print rows with consistent categories only
+print(airlines[~cat_clean_rows])
