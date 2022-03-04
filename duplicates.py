e414b339251baac402b8dbfdaa67c243666fae38
Perizat Menard <perizat.m@haystacks.ai>
2022-03-04 15:53:22 -0500

    treating duplicates

---
+++ duplicates.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+ride_sharing = pd.read_csv("/Users/perizatmenard/Documents/advanced_python/cleaning_data_with_pandas/data/ride_sharing_new.csv")
+
+# Drop complete duplicates from ride_sharing
+ride_dup = ride_sharing.drop_duplicates()
+
+# Create statistics dictionary for aggregation function
+statistics = {'user_birth_year': 'min', 'duration': 'mean'}
+
+# Group by ride_id and compute new statistics
+ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
+
+# Find duplicated values again
+duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
+duplicated_rides = ride_unique[duplicates == True]
+
+# Assert duplicates are processed
+assert duplicated_rides.shape[0] == 0
