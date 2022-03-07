84077a8191eeae9782360f4998e60e5c3cf957f2
Perizat Menard <perizat.m@haystacks.ai>
2022-03-07 11:26:29 -0500

    removing white spaces with strip()

---
+++ removing_white_space.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+airlines = pd.read_csv("/Users/perizatmenard/Documents/advanced_python/cleaning_data_with_pandas/data/airlines_final.csv")
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower()
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_size'].unique())
+print(airlines['dest_region'].unique())
+
